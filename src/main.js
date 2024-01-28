288eb176d9cafd6e5bd61e5b0e190409ed2c1769
HelloWorld7894 <danpojhan@protonmail.com>
2024-01-28 12:20:29 +0100

    added jQuery

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/*
 const { invoke } = window.__TAURI__.tauri;
 
 let greetInputEl;
@@ -7,12 +8,28 @@ async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
 }
+*/
+var position = undefined
+var top_pad = 20
 
-window.addEventListener("DOMContentLoaded", () => {
+$(document).ready(() => {
+  let joystick = $("#top-circle")
+  position = joystick.position();
+  joystick.css({top: position.top + top_pad, left: position.left, position:'absolute'});
+
+  joystick.draggable({
+    containment: "parent",
+    stop: function(event, ui){
+      console.log("stop event invoked")
+      joystick.css({top: position.top + top_pad, left: position.left, position:'absolute'});
+    }
+  })
+})
+  /*
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
   document.querySelector("#greet-form").addEventListener("submit", (e) => {
     e.preventDefault();
     greet();
   });
-});
+  */
