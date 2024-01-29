0b4851a11c48b36cec9c54afbf3798d3f8fc0986
HelloWorld7894 <danpojhan@protonmail.com>
2024-01-29 19:34:08 +0100

    frontend to backend communication

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,46 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+//message receivers
+
+//settings
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn greet() {
+  println!("I was invoked from JS!");
 }
+
+//control
+#[tauri::command]
+fn move_drone(side: String) {
+  println!("move {}", side)
+}
+
+#[tauri::command]
+fn rotate_drone(rot_side: i32) {
+  println!("I was invoked from JS!");
+}
+
+#[tauri::command]
+fn take_off() {
+  println!("I was invoked from JS!");
+}
+
+#[tauri::command]
+fn land() {
+  println!("I was invoked from JS!");
+}
+
+#[tauri::command]
+fn move_joystick(coords: [i32; 2]) {
+  println!("I was invoked from JS!");
+}
+
+//project
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    //messsage handlers
+    .invoke_handler(tauri::generate_handler![greet, move_drone])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,8 @@
-/*
-const { invoke } = window.__TAURI__.tauri;
-
-let greetInputEl;
-let greetMsgEl;
-
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
-*/
 var position = undefined
 var top_pad = 20
 
-$(document).ready(() => {
+$(document).ready(function(){
+
   let joystick = $("#top-circle")
   position = joystick.position();
   joystick.css({top: position.top + top_pad, left: position.left, position:'absolute'});
@@ -20,16 +10,7 @@ $(document).ready(() => {
   joystick.draggable({
     containment: "parent",
     stop: function(event, ui){
-      console.log("stop event invoked")
       joystick.css({top: position.top + top_pad, left: position.left, position:'absolute'});
     }
   })
 })
-  /*
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-  */
