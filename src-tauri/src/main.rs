5a96f4743b0e548506e9cc260014ff7e077e09f9
HelloWorld7894 <danpojhan@protonmail.com>
2024-01-30 13:42:55 +0100

    sending info about drone status

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,34 +1,46 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::borrow::Borrow;
 use std::io::{Read, Write};
-use std::net::{TcpStream};
+use std::net::TcpStream;
 
 //variables
 const ADDR: &str = "127.0.0.1";
 const PORT: &str = "9001";
-#[derive(Debug)]
-enum TcpOutput {
-    Out1(TcpStream),
-    Out2(bool)
+
+// the payload type must implement `Serialize` and `Clone`.
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+  message: String,
 }
 
+//glob variables (TODO: this is not really the safe way))
+static mut INITIAL_CONNECTION: bool = false;
+static mut ALREADY_SENT_MESSAGE: bool = false;
 
 //message receivers
-
+ 
 //settings
 #[tauri::command]
 fn connect_to_drone() {
   println!("I was invoked from JS!");
 
-  //TCP communication init
+  //TCP communiucation init
   let result = TcpStream::connect(format!("{ADDR}:{PORT}"));
-  let tcp_stream = match result {
-      Ok(tcp_stream) => TcpOutput::Out1(tcp_stream),
-      Err(e) => TcpOutput::Out2(false),
+  let mut tcp_stream = match result {
+      Ok(tcp_stream) => {
+        //drone connection approved
+        unsafe {INITIAL_CONNECTION = true;}
+
+        tcp_stream
+      },
+      Err(e) => {
+        //drone connection refused
+        unsafe {INITIAL_CONNECTION = false;}
+
+        return println!("connection refused: {}", e)
+      },
   };
-  TcpOutput::borrow(&self)
 
   tcp_stream.write_all(b"Hello server").unwrap();
   tcp_stream.flush().unwrap();
@@ -65,12 +77,28 @@ fn move_joystick(coords: [i32; 2]) {
   println!("move_joystick: {} {}", coords[0], coords[1]);
 }
 
+#[tauri::command]
+fn check_conn() -> String {
+  unsafe {
+    if ALREADY_SENT_MESSAGE == false{
+      ALREADY_SENT_MESSAGE = true;
+      match INITIAL_CONNECTION{
+        true => "connected".into(),
+        false => "not_connected".into(),
+      }
+    }
+    else {
+      return "blank".into(); //sending blank message
+    }
+  }
+}
+
 //project
 
 fn main() {
   tauri::Builder::default()
   //messsage handlers
-  .invoke_handler(tauri::generate_handler![connect_to_drone, move_drone, rotate_drone, take_off, land, move_joystick])
+  .invoke_handler(tauri::generate_handler![connect_to_drone, move_drone, rotate_drone, take_off, land, move_joystick, check_conn])
   .run(tauri::generate_context!())
   .expect("error while running tauri application");
 }
