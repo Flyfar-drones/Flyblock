7c6ae9f050b30dec3bb131e1963a07888ebeae28
HelloWorld7894 <danpojhan@protonmail.com>
2024-01-31 17:05:11 +0100

    andri má balls

--- src/flyblock-editor.js
+++ src/flyblock-editor.js
@@ -1,33 +1,49 @@
+var is_dragged = false
+
+function check_overlay(div1, div2){
+    var div1Offset = div1.offset();
+    var div2Offset = div2.offset();
+
+    // Check if div1 is on top of div2
+    if (!(
+        div1Offset.top + div1.height() <= div2Offset.top ||
+        div1Offset.top >= div2Offset.top + div2.height() ||
+        div1Offset.left + div1.width() <= div2Offset.left ||
+        div1Offset.left >= div2Offset.left + div2.width()
+    )) {
+        return true
+    }
+    else{
+        return false
+    }
+}
+
 $(document).ready(function(){
-    //do something
     $(".block").draggable({
         scroll: false,
-        drag: function (event, ui) {
-            //$(this).clone().appendTo(".codepanel-block");
-        },
         stop: function (event, ui){
-            var is_on_top = false
 
             var div1 = $(this)
             var div2 = $("#block-list")
+            var div_panel = $(".codepanel-block")
 
-            var div1Offset = div1.offset();
-            var div2Offset = div2.offset();
+            if (check_overlay(div1, div_panel)){
+                var clone = $(this).clone().appendTo(".codepanel-block")
+                clone.removeAttr("style")
+                clone.draggable({
+                    containment: ".codepanel-block"
+                })
 
-            // Check if div1 is on top of div2
-            if (!(
-                div1Offset.top + div1.height() <= div2Offset.top ||
-                div1Offset.top >= div2Offset.top + div2.height() ||
-                div1Offset.left + div1.width() <= div2Offset.left ||
-                div1Offset.left >= div2Offset.left + div2.width()
-            )) {
-                is_on_top = true
-            }
-
-            if (is_on_top){
-                $(this).removeAttr('style');
+                $(this).removeAttr("style");
                 $(this).attr("style", "position: relative;")
             }
+            
+            if (check_overlay(div1, div2)){
+                $(this).removeAttr("style");
+                $(this).attr("style", "position: relative;")
+            }
+
+            is_dragged = false
         }
     })
 })
