6a5ea8c3e900b4d669f8f1dae2715ada6e4b6982
HelloWorld7894 <danpojhan@protonmail.com>
2024-02-03 14:05:12 +0100

    still working on getting data

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,19 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::borrow::Borrow;
 use std::io::{Read, Write};
-use std::process;
 use std::time::Duration;
 use std::net::TcpStream;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use lazy_static::lazy_static;
-use tauri::State;
-use std::sync::{Mutex, MutexGuard, Arc};
-use std::mem::drop;
-use std::net::Shutdown;
+use std::sync::{Mutex, Arc};
 use tauri::Manager;
-use std::collections::HashMap;
 use serde_json::Value;
 
 //info
@@ -56,46 +49,6 @@ fn send_data(tcp_stream: Arc<Mutex<TcpStream>>, input: String) -> String{
 
 //message receivers
 
-//settings
-fn connect_to_drone(tcp_stream: TcpStream) {
-
-  println!("I was invoked from JS!");
-  unsafe {
-    ALREADY_SENT_MESSAGE = false;
-
-    //let server_response = send_data(tcp_stream, "hello".to_string());
-    //println!("Server says: {}", server_response);
-  }
-}
-
-#[tauri::command]
-fn send_connection_data() -> String{
-  unsafe {
-    if CONNECTION{
-      "connected".into()
-    }
-    else{
-      "not_connected".into()
-    }
-  }
-}
-
-#[tauri::command]
-fn send_test_data(tcp_stream: TcpStream){
-  /*
-  let server_response = send_data(tcp_stream, "ledon".to_string());
-  let response = server_response.as_str();
-
-  println!("Server says: {}", server_response);
-
-  match response{
-    "ok" => "ok".to_string().into(),
-    "not_ok" => "not_ok".to_string().into(),
-    &_ => return "random_error".to_string()
-  }
-   */
-}
-
 fn check_conn() -> String {
   unsafe {
     println!("{}", ALREADY_SENT_MESSAGE);
@@ -129,23 +82,19 @@ fn main() {
       let data = event.payload().unwrap();
       let deserialized: Value = serde_json::from_str(&data).unwrap();
       
-      let command_type: String = deserialized["command"].to_string();
+      let command_type = deserialized["command"].as_str().unwrap();
       let command_value: String = deserialized["value"].to_string();
-      
-      match command_type.as_str(){
-        "connect-to" => {
+      println!("{}", command_type.to_string());
+
+      match command_type{
+        "connect-to" | "rotate" | "move-joystick" => {
           send_data(tcp_stream.clone(), command_value);
         },
-        "move" => {
-          send_data(tcp_stream.clone(), command_value);
-        }
-        &_ => println!("tvoje matinka")
+        "take-off" | "land" => {
+          send_data(tcp_stream.clone(), command_type.to_string());
+        },
+        _ => println!("tvoje matinka")
       }
-    });
-    let id_test = app.listen_global("app", |event| {
-      let data = event.payload().unwrap();
-      let deserialized: Value = serde_json::from_str(&data).unwrap();
-      println!("{}", deserialized["command"])
     });
 
     // emit the `event-name` event to all webview windows on the frontend
